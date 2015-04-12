8233ca341720fab0d9089a63cee68ea8730112ce
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2015-04-12 12:59:28 +0800

    添加auto_max和auto_max宏
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

--- include/common.h
+++ include/common.h
@@ -16,11 +16,28 @@
 #define EXTERN_C  extern "C"
 
 
+#ifndef auto_max            // 这里将宏替换a的计算结果先保存至_a中
+#define auto_max(a,b)       ({              \
+                            auto _a = (a);  \
+                            auto _b = (b);  \
+                            (_a > _b) ? _a : _b; })
+#endif
+
+
+#ifndef auto_min
+#define auto_min(a,b)       ({              \
+                            auto _a = (a);  \
+                            auto _b = (b);  \
+                            (_a < _b) ? _a : _b; })
+#endif
+
+
 template<class T> inline
 typename ::std::decay<T>::type decay_type(T&& arg)
 {
     return ::std::forward<T>(arg);
 }
+
 
 struct function_wapper
 {
