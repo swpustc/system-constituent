c86f234acb569a8d5dff45eb04d94f516c36d39f
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2015-05-01 23:31:59 +0800

    添加链接文件
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

---
+++ include/link_system_constituent.h
@@ -0,0 +1,27 @@
+/**********************************************************
+* 链接
+* 支持平台：Windows; Linux
+* 编译环境：VS2010+; g++
+* 创建时间：2015-05-01 （宋万鹏）
+* 最后修改：2015-05-01 （宋万鹏）
+***********************************************************/
+
+#pragma once
+
+#ifdef _MSC_VER
+
+#ifdef _DEBUG       /* _DEBUG */
+#  ifdef _M_X64     /* _M_X64 */
+#    pragma comment(lib, "Debug/x64/system.lib")
+#  else             /* _M_X86 */
+#    pragma comment(lib, "Debug/system.lib")
+#  endif
+#else               /* NDEBUG */
+#  ifdef _M_X64     /* _M_X64 */
+#    pragma comment(lib, "x64/system.lib")
+#  else             /* _M_X86 */
+#    pragma comment(lib, "system.lib")
+#  endif
+#endif
+
+#endif /* _MSC_VER */
