43eca3d86be3363c1d3681de76d1e39e4e2196fa
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2015-05-10 16:32:46 +0800

    添加打开串口程序
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

--- include/serial_port.h
+++ include/serial_port.h
@@ -9,3 +9,49 @@
 #pragma once
 
 #include "common.h"
+#include "safe_object.h"
+
+
+class serial_port
+{
+private:
+    // 串口句柄
+    SAFE_HANDLE_OBJECT m_comm;
+
+    // 已验证过的串口名
+    SYSCONAPI bool __open(const wchar_t* portname);
+    bool __open(const ::std::wstring& portname){ return __open(portname.c_str()); }
+    bool _open(const char* portname){ return _open(convert_default_unicode.from_bytes(portname)); }
+    bool _open(const ::std::string& portname){ return _open(convert_default_unicode.from_bytes(portname)); }
+    bool _open(::std::string&& portname){ return _open(convert_default_unicode.from_bytes(::std::move(portname))); }
+    SYSCONAPI bool _open(const wchar_t* portname);
+    SYSCONAPI bool _open(const ::std::wstring& portname);
+    SYSCONAPI bool _open(::std::wstring&& portname);
+    SYSCONAPI bool _open(int port_number);
+    bool _open(unsigned int port_number){ return _open((int)(port_number)); }
+
+
+public:
+    serial_port() = default;
+    serial_port(const serial_port&) = delete;
+    serial_port& operator=(const serial_port&) = delete;
+
+    // 打开串口：portname可以为串口名(string|pointer)或串口序号(int 1,2,3...)
+    template<class T> bool open(T&& portname){ return _open(::std::forward<T>(portname)); }
+    // 串口是否已打开
+    bool is_open()
+    {
+        return m_comm.is_open();
+    }
+    // 关闭串口
+    bool close()
+    {
+        if (is_open())
+        {
+            m_comm.close();
+            return true;
+        }
+        else
+            return false;
+    }
+};
--- src/serial_port.cpp
+++ src/serial_port.cpp
@@ -8,5 +8,64 @@
 
 #include "common.h"
 #include "serial_port.h"
+#include <tchar.h>
+#include <Windows.h>
 
 using namespace std;
+
+
+static const wstring comm_name_prefix = L"\\\\.\\";
+
+// 已验证过的串口名
+bool serial_port::__open(const wchar_t* portname)
+{
+    m_comm = CreateFileW(portname, GENERIC_READ | GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, 0, nullptr);
+    if (!is_open())
+        return false;
+    if (!SetCommMask(m_comm, EV_RXCHAR | EV_TXEMPTY))
+        return false;
+    // 设置输入输出缓冲区大小
+    if (!SetupComm(m_comm, 1024, 1024))
+        return false;
+    //  清除状态位
+    if (!PurgeComm(m_comm, PURGE_TXABORT | PURGE_RXABORT | PURGE_TXCLEAR | PURGE_RXCLEAR))
+        return false;
+    COMMTIMEOUTS comm_timeouts = { 0 };
+    if (!SetCommTimeouts(m_comm, &comm_timeouts))
+        return false;
+    return true;
+}
+
+
+bool serial_port::_open(const wchar_t* portname)
+{
+    if (wcsncmp(portname, comm_name_prefix.c_str(), comm_name_prefix.size()) == 0)
+        return __open(portname);
+    else
+        return __open(comm_name_prefix + portname);
+}
+
+bool serial_port::_open(const ::std::wstring& portname)
+{
+    if (portname.compare(0, 4, comm_name_prefix) == 0)
+        return __open(portname.c_str());
+    else
+        return __open(comm_name_prefix + portname);
+}
+
+bool serial_port::_open(::std::wstring&& portname)
+{
+    if (portname.compare(0, 4, comm_name_prefix) == 0)
+        return __open(portname.c_str());
+    else
+        return __open(comm_name_prefix + move(portname));
+}
+
+bool serial_port::_open(int port_number)
+{
+    if (port_number <= 0 || port_number > 32)
+        return false;
+    wchar_t name[10];
+    wsprintfW(name, L"\\\\.\\COM%d", port_number);
+    return __open(name);
+}
