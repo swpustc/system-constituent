8764c3653e55695f7528fdbe974cf9492c9d7a74
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2015-05-29 09:11:50 +0800

    修改导出宏位置到Version头文件中
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

--- include/common.h
+++ include/common.h
@@ -30,46 +30,7 @@
 #include <sys/time.h>
 #endif // #if defined(_WIN32) || defined(WIN32)
 
-
-#define EXTERN_C  extern "C"
-
-#ifndef _CRT_STRINGIZE
-#define __CRT_STRINGIZE(_Value) #_Value
-#define _CRT_STRINGIZE(_Value) __CRT_STRINGIZE(_Value)
-#endif  /* _CRT_STRINGIZE */
-
-#ifndef _CRT_WIDE
-#define __CRT_WIDE(_String) L ## _String
-#define _CRT_WIDE(_String) __CRT_WIDE(_String)
-#endif  /* _CRT_WIDE */
-
-#ifndef _CRT_APPEND
-#define __CRT_APPEND(_Value1, _Value2) _Value1 ## _Value2
-#define _CRT_APPEND(_Value1, _Value2) __CRT_APPEND(_Value1, _Value2)
-#endif  /* _CRT_APPEND */
-
-#ifndef _T
-#ifdef _UNICODE
-#define __TXT(x)    L ## x
-#else  /* _UNICODE */
-#define __TXT(x)    x
-#endif  /* _UNICODE */
-#define _T(x)       __TXT(x)
-#endif  /* _T */
-
-#ifdef _WIN32
-#  ifdef SYSCON_EXPORT
-#    define SYSCONAPI_EXTERN    extern __declspec(dllexport)
-#    define SYSCONAPI           __declspec(dllexport)
-#  else
-#    define SYSCONAPI_EXTERN    extern __declspec(dllimport)
-#    define SYSCONAPI           __declspec(dllimport)
-#  endif
-#endif
-#ifndef SYSCONAPI
-#  define SYSCONAPI_EXTERN      extern
-#  define SYSCONAPI
-#endif
+#include "system_constituent_version.h"
 
 
 #if defined(_WIN32) || defined(WIN32)
