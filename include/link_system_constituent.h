28eb5f1ed50f592ea82310fd0500b678b40235f7
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2015-05-31 14:28:22 +0800

    修改链接文件位置
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

--- include/link_system_constituent.h
+++ include/link_system_constituent.h
@@ -15,13 +15,13 @@
 #  ifdef _M_X64     /* _M_X64 */
 #    pragma comment(lib, "Debug/x64/system" _CRT_STRINGIZE(SYSTEM_CONSTITUENT_VERSION) ".lib")
 #  else             /* _M_X86 */
-#    pragma comment(lib, "Debug/system" _CRT_STRINGIZE(SYSTEM_CONSTITUENT_VERSION) ".lib")
+#    pragma comment(lib, "Debug/Win32/system" _CRT_STRINGIZE(SYSTEM_CONSTITUENT_VERSION) ".lib")
 #  endif
 #else               /* NDEBUG */
 #  ifdef _M_X64     /* _M_X64 */
 #    pragma comment(lib, "x64/system" _CRT_STRINGIZE(SYSTEM_CONSTITUENT_VERSION) ".lib")
 #  else             /* _M_X86 */
-#    pragma comment(lib, "system" _CRT_STRINGIZE(SYSTEM_CONSTITUENT_VERSION) ".lib")
+#    pragma comment(lib, "Win32/system" _CRT_STRINGIZE(SYSTEM_CONSTITUENT_VERSION) ".lib")
 #  endif
 #endif
 
