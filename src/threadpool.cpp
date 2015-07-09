ddca199d1347401d416fd6f06a8badb82731ff03
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2015-07-09 12:46:35 +0800

    捕获C++异常
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

--- src/threadpool.cpp
+++ src/threadpool.cpp
@@ -17,10 +17,10 @@ template<> inline size_t threadpool<true>::pre_run(HANDLE pause_event, HANDLE re
         {
             return run(pause_event, resume_event);
         }
-        catch (::std::function<void()>& function_object)
+        catch (function<void()>& function_object)
         {
             debug_output<true>(_T(__FILE__), _T('('), __LINE__, _T("): "), function_object.target_type().name());
-            m_exception_tasks.push_back(::std::move(function_object));
+            m_exception_tasks.push_back(move(function_object));
         }
     }
 }
@@ -33,7 +33,7 @@ template<> inline size_t threadpool<false>::pre_run(HANDLE pause_event, HANDLE r
 
 
 // 运行一条任务，返回任务队列中是否还有任务[true:有任务; false:没任务]，捕获异常
-template<> inline bool threadpool<true>::run_task(::std::pair<::std::function<void()>, size_t>&& task_val)
+template<> inline bool threadpool<true>::run_task(pair<function<void()>, size_t>&& task_val)
 {
     // 任务队列中有任务未处理，发送线程启动通知
     if (task_val.second > 1)
@@ -44,9 +44,14 @@ template<> inline bool threadpool<true>::run_task(::std::pair<::std::function<vo
         {
             task_val.first();
         }
+        catch (exception& e)
+        {
+            debug_output<true>(_T(__FILE__), _T('('), __LINE__, _T("): "), e.what(), " | ", task_val.first.target_type().name());
+            m_exception_tasks.push_back(move(task_val.first));
+        }
         catch (...)
         {
-            throw ::std::move(task_val.first);
+            throw move(task_val.first);
             return false;
         }
         m_task_completed++;
@@ -55,7 +60,7 @@ template<> inline bool threadpool<true>::run_task(::std::pair<::std::function<vo
 }
 
 // 运行一条任务，返回任务队列中是否还有任务[true:有任务; false:没任务]，不捕获异常
-template<> inline bool threadpool<false>::run_task(::std::pair<::std::function<void()>, size_t>&& task_val)
+template<> inline bool threadpool<false>::run_task(pair<function<void()>, size_t>&& task_val)
 {
     // 任务队列中有任务未处理，发送线程启动通知
     if (task_val.second > 1)
