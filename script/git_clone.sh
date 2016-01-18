989e4aa5d981709747e980c96e738cd7d7f971d4
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2016-01-18 18:17:12 +0800

    指定检出master分支
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

--- script/git_clone.sh
+++ script/git_clone.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-git clone . "$1" >/dev/null 2>&1
+git clone . "$2" --branch $1 >/dev/null 2>&1
 
-GIT_DIR="$1/.git"
-GIT_WORK_TREE="$1"
+GIT_DIR="$2/.git"
+GIT_WORK_TREE="$2"
 
 commit=$(git show master | awk 'NR==1,NR==2'| grep commit | awk 'NR==1' | awk '{print $2}')
 echo commit: $commit
-touch-git "$1" >/dev/null 2>&1
+touch-git "$2" >/dev/null 2>&1
--- script/gitlab-ci.bat
+++ script/gitlab-ci.bat
@@ -9,7 +9,7 @@ FOR /F "tokens=*" %%I IN ('CALL "%~dp0\run_bash" "%~dp0\base_name.sh" "%NAME:~0,
 REM Git fetch
 IF     EXIST "E:\project\runner\%NAME%" CALL "%~dp0\run_bash" "%~dp0\git_fetch.sh" "%~dp0.." "E:\project\runner\%NAME%"
 REM Git clone
-IF NOT EXIST "E:\project\runner\%NAME%" CALL "%~dp0\run_bash" "%~dp0\git_clone.sh" "E:\project\runner\%NAME%"
+IF NOT EXIST "E:\project\runner\%NAME%" CALL "%~dp0\run_bash" "%~dp0\git_clone.sh" master "E:\project\runner\%NAME%"
 REM Remove output files
 DEL /F /A /Q "E:\project\runner\master\bin\Release\x64\%OUT%*" >NUL 2>&1
 REM Build
