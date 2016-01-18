7335f30791ddbbc48f017220c344ba430f3a5b2f
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2016-01-18 16:39:22 +0800

    fix bug
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

--- script/gitlab-ci.bat
+++ script/gitlab-ci.bat
@@ -2,6 +2,7 @@
 CHCP 65001
 REM Find project name
 SET "NAME=%~dp0"
+SET "OUT=system"
 FOR /F "tokens=*" %%I IN ('CALL "%~dp0\run_bash" "%~dp0\base_name.sh" "%NAME:~0,-8%"') DO (
   SET "NAME=%%I"
 )
@@ -10,11 +11,11 @@ IF     EXIST "E:\project\runner\%NAME%" CALL "%~dp0\run_bash" "%~dp0\git_fetch.s
 REM Git clone
 IF NOT EXIST "E:\project\runner\%NAME%" CALL "%~dp0\run_bash" "%~dp0\git_clone.sh" "E:\project\runner\%NAME%"
 REM Remove output files
-DEL /F /A /Q "E:\project\runner\master\bin\Release\x64\%NAME%*" >NUL 2>&1
+DEL /F /A /Q "E:\project\runner\master\bin\Release\x64\%OUT%*" >NUL 2>&1
 REM Build
 IF EXIST "E:\project\runner\%NAME%\install.bat" CALL "E:\project\runner\%NAME%\install.bat"
 CALL "E:\project\runner\%NAME%\build_release_x64.bat"
 REM Test
-IF EXIST "E:\project\runner\master\bin\Release\x64\%NAME%*.exe" EXIT /B 0
-IF EXIST "E:\project\runner\master\bin\Release\x64\%NAME%*.dll" EXIT /B 0
+IF EXIST "E:\project\runner\master\bin\Release\x64\%OUT%*.exe" EXIT /B 0
+IF EXIST "E:\project\runner\master\bin\Release\x64\%OUT%*.dll" EXIT /B 0
 EXIT /B 1
