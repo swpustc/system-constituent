54147b65af9444597786f58e242fc65a8ff8bb2b
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2016-01-18 19:14:17 +0800

    fix bug: 使用pull
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

--- script/git_fetch.sh
+++ script/git_fetch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-GIT_DIR="$1/.git" GIT_WORK_TREE="$1" git reset --hard origin/master
+REMOTE=$(GIT_DIR="$1/.git" GIT_WORK_TREE="$1" git remote -v | grep origin | grep fetch | awk '{print $2}')
 
 GIT_DIR="$2/.git"
 GIT_WORK_TREE="$2"
 
-git remote set-url origin $1 >/dev/null 2>&1
-git fetch
+git remote set-url origin REMOTE >/dev/null 2>&1
+git pull
 
 commit=$(git show master | awk 'NR==1,NR==2'| grep commit | awk 'NR==1' | awk '{print $2}')
 echo commit: $commit
