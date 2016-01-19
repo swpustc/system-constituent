22e4733feac221530ad75133e7bf820ab33fd51c
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2016-01-19 10:51:04 +0800

    指定检出的分支
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

--- script/git_fetch.sh
+++ script/git_fetch.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 REMOTE=$(GIT_DIR="$1/.git" GIT_WORK_TREE="$1" git remote -v | grep origin | grep fetch | awk '{print $2}')
+COMMIT=$(GIT_DIR="$1/.git" GIT_WORK_TREE="$1" git show | awk 'NR==1,NR==2'| grep commit | awk 'NR==1' | awk '{print $2}')
 
 GIT_DIR="$2/.git"
 GIT_WORK_TREE="$2"
 cd $2
 
 git remote set-url origin $REMOTE >/dev/null 2>&1
-git checkout -f                   >/dev/null 2>&1
-git pull
+git fetch --prune
+git checkout $COMMIT -f           >/dev/null 2>&1
 
 commit=$(git show | awk 'NR==1,NR==2'| grep commit | awk 'NR==1' | awk '{print $2}')
 echo commit: $commit
