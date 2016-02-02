180ece680b4b30394fb9f7ad301c02b790f5366c
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2016-02-02 15:51:23 +0800

    只枚举根目录下的SLN
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

--- build_release_x64.bat
+++ build_release_x64.bat
@@ -7,8 +7,8 @@ call "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat" amd64
 
 if /i not "%VisualStudioVersion%" == "12.0" goto :explain
 
-for /F %%i in ('dir /s /b "%~dp0\*.sln"') DO (
-  set "SLN_DIR=%%i"
+for /F %%i in ('DIR /B "%~dp0*.sln"') DO (
+  set "SLN_DIR=%~dp0%%i"
 )
 set "VS_VERSION=2013"
 
