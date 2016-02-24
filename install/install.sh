82fb0388f4b3e0fa882109ae13f8f599f569fbaf
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2016-02-24 12:09:50 +0800

    检查props文件并生成版本号
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

--- install/install.sh
+++ install/install.sh
@@ -4,6 +4,7 @@ project=$1
 in_path=$2
 outpath=$3
 verfile=$4
+props_f=$5
 
 
 data_path=$outpath/packages/$project/data
@@ -30,7 +31,18 @@ ver_maj=$(cat "$verfile" | grep 'VERSION_MAJOR ' | awk 'NR==1' | awk '{printf $(
 ver_min=$(cat "$verfile" | grep 'VERSION_MINOR ' | awk 'NR==1' | awk '{printf $(NF)}')
 ver_pt1=$(cat "$verfile" | grep 'VERSION_POINT ' | awk 'NR==1' | awk '{printf $(NF)}')
 ver_pt2=$(cat "$verfile" | grep 'VERSION_POINT2' | awk 'NR==1' | awk '{printf $(NF)}')
-ver=$ver_maj.$ver_min.$ver_pt1.$ver_pt2
+if [ -f "$props_f" ] ; then
+  ver_who=$(cat "$props_f" | grep 'GlobalWholeProgramOptimization' | awk 'NR==2' | awk -F\> '{printf $2}' | awk -F\< '{printf $1}')
+  if [ "$ver_who"x = "true"x ] ; then
+    ver=$ver_maj.$ver_min.$ver_pt1.$ver_pt2-2
+  elif [ "$ver_who"x = "false"x ] ; then
+    ver=$ver_maj.$ver_min.$ver_pt1.$ver_pt2-1
+  else
+    ver=$ver_maj.$ver_min.$ver_pt1.$ver_pt2
+  fi
+else
+  ver=$ver_maj.$ver_min.$ver_pt1.$ver_pt2
+fi
 
 date=$(GIT_DIR="$in_path/.git" GIT_WORK_TREE="$in_path" git show -s --pretty=format:%ci | awk 'NR==1' | awk '{printf $1}')
 
