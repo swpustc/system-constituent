9165799173b0c69359600b0849265f31621d63b3
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2016-02-29 12:14:22 +0800

    添加pdb文件
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

--- install/add.sh
+++ install/add.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 project=$1
-in_file=$2
-outpath=$3
+outpath=$2
+in_file=$3
+pdbfile=$4
 
 
 data_bin_path=$outpath/packages/$project/data/bin/
+data_pdb_path=$outpath/packages/$project/data/pdb/
 
 if [ -f "$in_file" ] ; then
   cp -p "$in_file" "$data_bin_path"
+else
+  echo Error: File: "$in_file" not exist.
+  exit 1
+fi
+
+if [ -f "$pdbfile" ] ; then
+  cp -p "$pdbfile" "$data_pdb_path"
 else
   echo Error: File: "$in_file" not exist.
   exit 1
--- install/install.sh
+++ install/install.sh
@@ -24,6 +24,7 @@ else
 fi
 mkdir "$data_path"
 mkdir "$data_path/bin"
+mkdir "$data_path/pdb"
 mkdir "$meta_path"
 
 
@@ -31,17 +32,18 @@ ver_maj=$(cat "$verfile" | grep 'VERSION_MAJOR ' | awk 'NR==1' | awk '{printf $(
 ver_min=$(cat "$verfile" | grep 'VERSION_MINOR ' | awk 'NR==1' | awk '{printf $(NF)}')
 ver_pt1=$(cat "$verfile" | grep 'VERSION_POINT ' | awk 'NR==1' | awk '{printf $(NF)}')
 ver_pt2=$(cat "$verfile" | grep 'VERSION_POINT2' | awk 'NR==1' | awk '{printf $(NF)}')
+ver_tmp=$(GIT_DIR="$in_path/.git" GIT_WORK_TREE="$in_path" git show -s --pretty=format:%ct | awk 'NR==1')
 if [ -f "$props_f" ] ; then
   ver_who=$(cat "$props_f" | grep 'GlobalWholeProgramOptimization' | awk 'NR==2' | awk -F\> '{printf $2}' | awk -F\< '{printf $1}')
   if [ "$ver_who"x = "true"x ] ; then
-    ver=$ver_maj.$ver_min.$ver_pt1.$ver_pt2-2
+    ver=$ver_maj.$ver_min.$ver_pt1.$ver_pt2-$ver_tmp-2
   elif [ "$ver_who"x = "false"x ] ; then
-    ver=$ver_maj.$ver_min.$ver_pt1.$ver_pt2-1
+    ver=$ver_maj.$ver_min.$ver_pt1.$ver_pt2-$ver_tmp-1
   else
-    ver=$ver_maj.$ver_min.$ver_pt1.$ver_pt2
+    ver=$ver_maj.$ver_min.$ver_pt1.$ver_pt2-$ver_tmp
   fi
 else
-  ver=$ver_maj.$ver_min.$ver_pt1.$ver_pt2
+  ver=$ver_maj.$ver_min.$ver_pt1.$ver_pt2-$ver_tmp
 fi
 
 date=$(GIT_DIR="$in_path/.git" GIT_WORK_TREE="$in_path" git show -s --pretty=format:%ci | awk 'NR==1' | awk '{printf $1}')
