902a8e17eac13c9f23631411313a57987de21f65
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2016-02-29 12:43:39 +0800

    添加更新GIT_HEAD_MASTER脚本
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

---
+++ git_head.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+in_path=$1
+
+
+NEW_COMMIT=$(GIT_DIR="$in_path/.git" GIT_WORK_TREE="$in_path" git show -s --pretty=format:%h | awk 'NR==1' | awk '{printf $1}')
+
+if [ -f "$in_path/include/GIT_HEAD_MASTER" ] ; then
+  OLD_COMMIT=$(cat "$in_path/include/GIT_HEAD_MASTER" | awk 'NR==1' | awk '{printf $3}')
+fi
+
+
+if [ "$NEW_COMMIT"x != "$OLD_COMMIT"x ] ; then
+  echo "Update new GIT_HEAD_MASTER($NEW_COMMIT)..."
+  echo "#define GIT_HEAD_MASTER $NEW_COMMIT">"$in_path/include/GIT_HEAD_MASTER"
+fi
