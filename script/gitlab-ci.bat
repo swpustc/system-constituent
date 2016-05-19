b91b6fcfd1ea74da45b80a11a19464fcf7b9685e
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2016-05-19 18:41:50 +0800

    修改CI编译目录
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

--- script/gitlab-ci.bat
+++ script/gitlab-ci.bat
@@ -7,15 +7,15 @@ FOR /F "tokens=*" %%I IN ('CALL "%~dp0\run_bash" "%~dp0\base_name.sh" "%NAME:~0,
   SET "NAME=%%I"
 )
 REM Git clone
-IF NOT EXIST "E:\project\runner\%NAME%" CALL "%~dp0\run_bash" "%~dp0\git_clone.sh" master "E:\project\runner\%NAME%"
+IF NOT EXIST "E:\project\r\%NAME%" CALL "%~dp0\run_bash" "%~dp0\git_clone.sh" master "E:\project\r\%NAME%"
 REM Git fetch
-IF EXIST "E:\project\runner\%NAME%" CALL "%~dp0\run_bash" "%~dp0\git_fetch.sh" "%~dp0.." "E:\project\runner\%NAME%"
+IF EXIST "E:\project\r\%NAME%" CALL "%~dp0\run_bash" "%~dp0\git_fetch.sh" "%~dp0.." "E:\project\r\%NAME%"
 REM Remove output files
-DEL /F /A /Q "E:\project\runner\master\bin\Release\x64\%OUT%*" >NUL 2>&1
+DEL /F /A /Q "E:\project\r\master\bin\Release\x64\%OUT%*" >NUL 2>&1
 REM Build
-IF EXIST "E:\project\runner\%NAME%\install.bat" CALL "E:\project\runner\%NAME%\install.bat"
-CALL "E:\project\runner\%NAME%\build_release_x64.bat"
+IF EXIST "E:\project\r\%NAME%\install.bat" CALL "E:\project\r\%NAME%\install.bat"
+CALL "E:\project\r\%NAME%\build_release_x64.bat"
 REM Test
-IF EXIST "E:\project\runner\master\bin\Release\x64\%OUT%*.exe" EXIT /B 0
-IF EXIST "E:\project\runner\master\bin\Release\x64\%OUT%*.dll" EXIT /B 0
+IF EXIST "E:\project\r\master\bin\Release\x64\%OUT%*.exe" EXIT /B 0
+IF EXIST "E:\project\r\master\bin\Release\x64\%OUT%*.dll" EXIT /B 0
 EXIT /B 1
