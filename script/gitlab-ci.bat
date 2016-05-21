8c72a27e680e0e927b5814dc7dbd7f1964889ce5
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2016-05-21 12:16:41 +0800

    CI路径使用网络驱动器
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

--- script/gitlab-ci.bat
+++ script/gitlab-ci.bat
@@ -6,16 +6,19 @@ SET "OUT=system"
 FOR /F "tokens=*" %%I IN ('CALL "%~dp0\run_bash" "%~dp0\base_name.sh" "%NAME:~0,-8%"') DO (
   SET "NAME=%%I"
 )
+
+REM Create device
+IF NOT EXIST "X:\" NET USE X: \\localhost\project /PERSISTENT:YES
 REM Git clone
-IF NOT EXIST "E:\project\r\%NAME%" CALL "%~dp0\run_bash" "%~dp0\git_clone.sh" master "E:\project\r\%NAME%"
+IF NOT EXIST "X:\r\%NAME%" CALL "%~dp0\run_bash" "%~dp0\git_clone.sh" master "X:\r\%NAME%"
 REM Git fetch
-IF EXIST "E:\project\r\%NAME%" CALL "%~dp0\run_bash" "%~dp0\git_fetch.sh" "%~dp0.." "E:\project\r\%NAME%"
+IF     EXIST "X:\r\%NAME%" CALL "%~dp0\run_bash" "%~dp0\git_fetch.sh" "%~dp0.." "X:\r\%NAME%"
 REM Remove output files
-DEL /F /A /Q "E:\project\r\master\bin\Release\x64\%OUT%*" >NUL 2>&1
+DEL /F /A /Q "X:\r\master\bin\Release\x64\%OUT%*" >NUL 2>&1
 REM Build
-IF EXIST "E:\project\r\%NAME%\install.bat" CALL "E:\project\r\%NAME%\install.bat"
-CALL "E:\project\r\%NAME%\build_release_x64.bat"
+IF EXIST "X:\r\%NAME%\install.bat" CALL "X:\r\%NAME%\install.bat"
+CALL "X:\r\%NAME%\build_release_x64.bat"
 REM Test
-IF EXIST "E:\project\r\master\bin\Release\x64\%OUT%*.exe" EXIT /B 0
-IF EXIST "E:\project\r\master\bin\Release\x64\%OUT%*.dll" EXIT /B 0
+IF EXIST "X:\r\master\bin\Release\x64\%OUT%*.exe" EXIT /B 0
+IF EXIST "X:\r\master\bin\Release\x64\%OUT%*.dll" EXIT /B 0
 EXIT /B 1
