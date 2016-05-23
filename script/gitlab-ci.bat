ec7218b645127c651aeb0dd71cec540b4dd6d5a6
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2016-05-23 13:42:51 +0800

    输出错误返回值
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

--- script/gitlab-ci.bat
+++ script/gitlab-ci.bat
@@ -13,12 +13,6 @@ REM Git clone
 IF NOT EXIST "X:\r\%NAME%" CALL "%~dp0\run_bash" "%~dp0\git_clone.sh" master "X:\r\%NAME%"
 REM Git fetch
 IF     EXIST "X:\r\%NAME%" CALL "%~dp0\run_bash" "%~dp0\git_fetch.sh" "%~dp0.." "X:\r\%NAME%"
-REM Remove output files
-DEL /F /A /Q "X:\r\master\bin\Release\x64\%OUT%*" >NUL 2>&1
 REM Build
 IF EXIST "X:\r\%NAME%\install.bat" CALL "X:\r\%NAME%\install.bat"
 CALL "X:\r\%NAME%\build_release_x64.bat"
-REM Test
-IF EXIST "X:\r\master\bin\Release\x64\%OUT%*.exe" EXIT /B 0
-IF EXIST "X:\r\master\bin\Release\x64\%OUT%*.dll" EXIT /B 0
-EXIT /B 1
