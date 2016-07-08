32a27d6344784c13239251e9697fc5ebf585992c
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2016-07-08 18:20:39 +0800

    修正<>转义
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

--- install/install.sh
+++ install/install.sh
@@ -32,17 +32,17 @@ ver_maj=$(cat "$verfile" | grep 'VERSION_MAJOR ' | awk 'NR==1' | awk '{printf $(
 ver_min=$(cat "$verfile" | grep 'VERSION_MINOR ' | awk 'NR==1' | awk '{printf $(NF)}')
 ver_pt1=$(cat "$verfile" | grep 'VERSION_POINT ' | awk 'NR==1' | awk '{printf $(NF)}')
 ver_pt2=$(cat "$verfile" | grep 'VERSION_POINT2' | awk 'NR==1' | awk '{printf $(NF)}')
-ver_tmp=$(GIT_DIR="$in_path/.git" GIT_WORK_TREE="$in_path" git show -s --pretty=format:%ct | awk 'NR==1' | sed 's/</\&lt;/g' | sed 's/>/\&gt;/g' | sed 's#/#\\/#g')
+ver_tmp=$(GIT_DIR="$in_path/.git" GIT_WORK_TREE="$in_path" git show -s --pretty=format:%ct | awk 'NR==1' | sed 's/</\\\&lt;/g' | sed 's/>/\\\&gt;/g' | sed 's#/#\\/#g')
 ver=$ver_maj.$ver_min.$ver_pt1.$ver_pt2.$ver_tmp
 if [ -f "$props_f" ] ; then
-  ver_who=$(cat "$props_f" | grep 'QtBuildVersion' | awk 'NR==1' | awk -F\> '{printf $2}' | awk -F\< '{printf $1}' | sed 's/</\&lt;/g' | sed 's/>/\&gt;/g' | sed 's#/#\\/#g')
+  ver_who=$(cat "$props_f" | grep 'QtBuildVersion' | awk 'NR==1' | awk -F\> '{printf $2}' | awk -F\< '{printf $1}' | sed 's/</\\\&lt;/g' | sed 's/>/\\\&gt;/g' | sed 's#/#\\/#g')
   if [ "$ver_who"x != ""x ] ; then
     ver=$ver.$ver_who
   fi
 fi
 
-date=$(GIT_DIR="$in_path/.git" GIT_WORK_TREE="$in_path" git show -s --pretty=format:%ci | awk 'NR==1' | awk '{printf $1}' | sed 's/</\&lt;/g' | sed 's/>/\&gt;/g' | sed 's#/#\\/#g')
-commit=$(GIT_DIR="$in_path/.git" GIT_WORK_TREE="$in_path" git show -s --pretty=format:%s | awk 'NR==1' | sed 's/</\&lt;/g' | sed 's/>/\&gt;/g' | sed 's#/#\\/#g')
+date=$(GIT_DIR="$in_path/.git" GIT_WORK_TREE="$in_path" git show -s --pretty=format:%ci | awk 'NR==1' | awk '{printf $1}' | sed 's/</\\\&lt;/g' | sed 's/>/\\\&gt;/g' | sed 's#/#\\/#g')
+commit=$(GIT_DIR="$in_path/.git" GIT_WORK_TREE="$in_path" git show -s --pretty=format:%s | awk 'NR==1' | sed 's/</\\\&lt;/g' | sed 's/>/\\\&gt;/g' | sed 's#/#\\/#g')
 
 cat "$in_path/install/package.xml"   | \
   sed "s/\$(DisplayName)/$project/g" | \
